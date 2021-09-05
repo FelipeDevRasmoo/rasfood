2ba7b5dfb5da53ca23e03d7413c952eef8b51251
FelipeDevRasmoo <felipe.alves@rasmoo.com>
2021-09-05 20:35:25 -0300

    cria primeira entidade

---
+++ src/main/java/br/com/rasmoo/restaurante/entity/Prato.java
@@ -0,0 +1,88 @@
+package br.com.rasmoo.restaurante.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "pratos")
+public class Prato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+    private String descricao;
+
+    private Boolean disponivel;
+
+    private BigDecimal valor;
+
+    @Column(name = "data_de_registro")
+    private LocalDateTime dataDeRegistro = LocalDateTime.now();
+
+    public Prato() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Boolean getDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(Boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public LocalDateTime getDataDeRegistro() {
+        return dataDeRegistro;
+    }
+
+    public void setDataDeRegistro(LocalDateTime dataDeRegistro) {
+        this.dataDeRegistro = dataDeRegistro;
+    }
+
+    @Override
+    public String toString() {
+        return "Prato{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", disponivel=" + disponivel +
+                ", valor=" + valor +
+                ", dataDeRegistro=" + dataDeRegistro +
+                '}';
+    }
+}
