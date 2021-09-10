a1cad4f648d2e18ebefdd88c05dc9e4941e4c98b
FelipeDevRasmoo <felipe.alves@rasmoo.com>
2021-09-10 00:53:46 -0300

    Executa testes CRUD

--- src/main/java/br/com/rasmoo/restaurante/dao/PratoDao.java
+++ src/main/java/br/com/rasmoo/restaurante/dao/PratoDao.java
@@ -22,7 +22,6 @@ public class PratoDao {
 
     public void cadastrar(final Prato prato) {
         this.entityManager.persist(prato);
-        System.out.println("Entidade cadastrada: " + prato);
     }
 
     public Prato consultar(final Integer id) {
--- src/main/java/br/com/rasmoo/restaurante/service/teste/PratoService.java
+++ src/main/java/br/com/rasmoo/restaurante/service/teste/PratoService.java
@@ -16,11 +16,27 @@ public class PratoService {
         risoto.setDisponivel(true);
         risoto.setValor(BigDecimal.valueOf(88.50));
 
+        Prato salmao = new Prato();
+        salmao.setNome("Salmão ao molho de maracuja");
+        salmao.setDescricao("Salmão grelhado ao molho de maracujá");
+        salmao.setDisponivel(true);
+        salmao.setValor(BigDecimal.valueOf(60.00));
+
         EntityManager entityManager = JPAUtil.getEntityManagerRasFood();
         PratoDao pratoDao = new PratoDao(entityManager);
         entityManager.getTransaction().begin();
         pratoDao.cadastrar(risoto);
-        entityManager.getTransaction().commit();
-        entityManager.close();
+        entityManager.flush();
+        pratoDao.cadastrar(salmao);
+        entityManager.flush();
+        System.out.println("O prato consultado foi: "+pratoDao.consultar(1));
+
+        pratoDao.excluir(salmao);
+        System.out.println("O prato consultado foi: "+pratoDao.consultar(2));
+
+        entityManager.clear();
+        risoto.setValor(BigDecimal.valueOf(75.50));
+        pratoDao.atualizar(risoto);
+        System.out.println("O prato consultado foi: "+pratoDao.consultar(1));
     }
 }
