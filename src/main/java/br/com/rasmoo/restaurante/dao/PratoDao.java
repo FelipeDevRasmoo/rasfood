64b210349e02f0aa4aba4e80741528646f46a664
FelipeDevRasmoo <felipe.alves@rasmoo.com>
2021-09-10 00:22:18 -0300

    demais metodos crud

--- src/main/java/br/com/rasmoo/restaurante/dao/PratoDao.java
+++ src/main/java/br/com/rasmoo/restaurante/dao/PratoDao.java
@@ -8,13 +8,33 @@ public class PratoDao {
 
     private EntityManager entityManager;
 
-    public PratoDao(EntityManager entityManager){
+    public PratoDao(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
 
-    public void cadastrar (Prato prato){
+    /*
+     * CRUD
+     * C = CREATE
+     * R = READ
+     * U = UPDATE
+     * D = DELETE
+     * */
+
+    public void cadastrar(final Prato prato) {
         this.entityManager.persist(prato);
-        System.out.println("Entidade cadastrada: "+prato);
+        System.out.println("Entidade cadastrada: " + prato);
+    }
+
+    public Prato consultar(final Integer id) {
+        return this.entityManager.find(Prato.class, id);
+    }
+
+    public void atualizar(final Prato prato){
+        this.entityManager.merge(prato);
+    }
+
+    public void excluir(final Prato prato){
+        this.entityManager.remove(prato);
     }
 
 }
