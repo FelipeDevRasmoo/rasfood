4e69bd81e2ec774b31e2937e471e78e74db49447
FelipeDevRasmoo <felipe.alves@rasmoo.com>
2021-09-13 21:44:55 -0300

    nome da tabela

--- src/main/java/br/com/rasmoo/restaurante/entity/Categoria.java
+++ src/main/java/br/com/rasmoo/restaurante/entity/Categoria.java
@@ -1,11 +1,9 @@
 package br.com.rasmoo.restaurante.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "categorias")
 public class Categoria {
 
     @Id
